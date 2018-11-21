5b29717df8fd7ea6b5fe17c2c3f3a2935c3e6d25
Vichearith Meas <44794938+vichym@users.noreply.github.com>
2018-11-21 16:27:53 -0600

    Initial commit

---
+++ main_index.py
@@ -0,0 +1,65 @@
+from PIL import Image
+import os
+
+
+def createThumbnails(pic, w, h):
+    newPic = pic.copy()
+    newPic.thumbnail((w,h))
+    return newPic
+
+
+def BWFolder():
+    path =  r'C:\Users\LYHENG-HD\PycharmProjects\isp'
+    newpath = createNewFolder("B&W", path)
+    for file in os.listdir('Images'):
+        newf = Image.open("Images\{}".format(str(file))).copy()
+        newf.convert(mode='L').save(newpath +'\{}_{}'.format("BW",file))
+
+
+def ThumnailsFolder():
+    path = r'C:\Users\LYHENG-HD\PycharmProjects\isp'
+    newpath = createNewFolder("Thumbnails", path)
+    for file in os.listdir('Images'):
+        thumnail= Image.open("Images\{}".format(file))
+        createThumbnails(thumnail, 220,300).save(newpath+'\{}_{}'.format('thumbnail',file))
+
+def createNewFolder(name, path):
+    newPath = path + '\{}'.format(name)
+    if not os.path.exists(newPath):
+        os.mkdir(newPath)
+    return newPath
+
+
+class Picture:
+
+    def __init__(self,*args):
+        self.image = Image.open(args[0])
+        self.thumList = {}
+        self.thumVersion = self.createThumbnail(250,300)
+
+    def createThumbnail(self, w, h):
+        name = (w,h)
+        newPic = self.image.copy() #.thumbnail((w,h))
+        newPic.thumbnail((w, h))
+        self.thumList[name] = newPic
+        return newPic
+
+
+
+if __name__ == '__main__':
+
+    # pic = Image.open('awesome.jpg')
+
+    # createBW()
+
+    # print('Images\{}'.format(file))
+    # createNewFolder('Vivid')
+    path = r'C:\Users\LYHENG-HD\PycharmProjects\isp\Images\wooster1.jpg'
+
+    # ThumnailsFolder()
+    newPic = Picture(path)
+    newPic.image.show()
+    newPic.createThumbnail(20,30)
+    print(newPic.thumList)
+    newPic.thumList[(20,30)].show()
+
