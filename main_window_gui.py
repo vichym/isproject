876d27ae473fb8e7f14c312054fdc615ccbac0c0
inamekazi <alejandroaguilar433@gmail.com>
2018-12-01 16:21:11 -0600

    first window

--- main_window_gui.py
+++ main_window_gui.py
@@ -1,26 +1,28 @@
 import tkinter as tk
-
+import PIL
+import PIL.Image as Image
+import PIL.ImageTk as ImageTk
 
 class Window1:
     def __init__(self):
         self.mainWin = tk.Tk()
+        self.mainWin.geometry("1250x600")
         label1 = tk.Label(self.mainWin, text="+", font="Arial 32 bold",height = 5, width = 10, borderwidth = 50)
         label1.grid(row=0, column=0)
 
-        label2 = tk.Label(self.mainWin, text = "+", font="Arial 18 bold", image = tk.Image.open(source o.))
-        label2.grid(row=1, column=0)
-        #
-        # label1 = tk.Label(self.mainWin, text="Type a phrase to be reversed", font="Arial 18 bold")
-        # label1.grid(row=0, column=0)
-        #
-        # label1 = tk.Label(self.mainWin, text="Type a phrase to be reversed", font="Arial 18 bold")
-        # label1.grid(row=0, column=0)
-        #
-        # label1 = tk.Label(self.mainWin, text="Type a phrase to be reversed", font="Arial 18 bold")
-        # label1.grid(row=0, column=0)
-        #
-        # label1 = tk.Label(self.mainWin, text="Type a phrase to be reversed", font="Arial 18 bold")
-        # label1.grid(row=0, column=0)
+        FileImage = Image.open("SampleImages/ImageFile.png")
+        self.file1Image = ImageTk.PhotoImage(FileImage)
+        label2 = tk.Label(self.mainWin, image = self.file1Image)
+        label2.grid(row=0, column=1)
+
+        label3 = tk.Label(self.mainWin, image = self.file1Image)
+        label3.grid(row=0, column=2)
+
+        label4 = tk.Label(self.mainWin, image = self.file1Image)
+        label4.grid(row=0, column=3)
+
+        label5 = tk.Label(self.mainWin, image = self.file1Image)
+        label5.grid(row=1, column=0)
     def run(self):
         self.mainWin.mainloop()
 
