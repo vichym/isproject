6c321487b2d68a37553b8c4b543c315fef7e34bd
ngonh <ngonhat352@gmail.com>
2018-12-01 15:09:50 -0600

    first window

--- main_window_gui.py
+++ main_window_gui.py
@@ -7,7 +7,7 @@ class Window1:
         label1 = tk.Label(self.mainWin, text="+", font="Arial 32 bold",height = 5, width = 10, borderwidth = 50)
         label1.grid(row=0, column=0)
 
-        label2 = tk.Label(self.mainWin, text = "+", font="Arial 18 bold")
+        label2 = tk.Label(self.mainWin, text = "+", font="Arial 18 bold", image = tk.Image.open())
         label2.grid(row=1, column=0)
         #
         # label1 = tk.Label(self.mainWin, text="Type a phrase to be reversed", font="Arial 18 bold")
